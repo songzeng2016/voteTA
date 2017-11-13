4fe35258d52c543774695d512e435ff19d21e1b8
songzeng2016 <songzeng62@gmail.com>
2017-11-13 18:04:01 +0800

    ‘完成筹码页面样式

--- voteApp/pages/index/index.js
+++ voteApp/pages/index/index.js
@@ -3,18 +3,7 @@
 const app = getApp()
 
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
+  data: {},
   onLoad: function () {
     if (app.globalData.userInfo) {
       this.setData({
