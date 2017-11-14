2ae5c708a38752f5622ae8c021c07fda7b9522ad
songzeng2016 <songzeng62@gmail.com>
2017-11-14 17:00:17 +0800

    完成等级页面样式

--- voteApp/pages/index/index.js
+++ voteApp/pages/index/index.js
@@ -3,14 +3,78 @@
 const app = getApp()
 
 Page({
-  data: {},
+  data: {
+    grade: {
+      leftList: [
+        {
+          desc: '一斗',
+          num: '0000-1999',
+          lbgc: '#53aadd',
+        },
+        {
+          desc: '二斗',
+          num: '0000-1999',
+          lbgc: '#53aadd',
+        },
+        {
+          desc: '三斗',
+          num: '0000-1999',
+          lbgc: '#53aadd',
+        },
+        {
+          desc: '四斗',
+          num: '0000-1999',
+          lbgc: '#53aadd',
+        },
+        {
+          desc: '五斗',
+          num: '0000-1999',
+          lbgc: '#2d71b6',
+        },
+        {
+          desc: '六斗',
+          num: '0000-1999',
+        },
+        {
+          desc: '七斗',
+          num: '0000-1999',
+        },
+        {
+          desc: '八斗',
+          num: '8000-9999',
+        },
+      ],
+      rightList: [
+        {
+          desc: '一车',
+          num: '0000-1999',
+        },
+        {
+          desc: '二车',
+          num: '0000-1999',
+        },
+        {
+          desc: '三车',
+          num: '0000-1999',
+        },
+        {
+          desc: '四车',
+          num: '0000-1999',
+        },
+        {
+          desc: '五车',
+          num: '0000-1999',
+        },
+      ]
+    }
+  },
   onLoad: function () {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -32,7 +96,7 @@ Page({
       })
     }
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
